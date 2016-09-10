61d0b0af616bc2ca8de99161646a75c08aeee49f
Nat! <nat@mulle-kybernetik.com>
2016-09-10 15:52:53 +0200

    massively improved readability of test script

--- run-test-common.sh
+++ run-test-common.sh
@@ -16,8 +16,8 @@ suppress_crashdumping()
 {
    local restore
 
-   case `uname` in
-      Darwin)
+   case "${UNAME}" in
+      darwin)
          restore="`defaults read com.apple.CrashReporter DialogType 2> /dev/null`"
          defaults write com.apple.CrashReporter DialogType none
          ;;
@@ -317,8 +317,8 @@ run_compiler()
 
 run_a_out()
 {
-   case "`uname`" in
-      Darwin)
+   case "${UNAME}" in
+      darwin)
          MULLE_OBJC_TEST_ALLOCATOR=1 \
 MallocStackLogging=1 \
 MallocScribble=1 \
@@ -356,7 +356,7 @@ check_compiler_output()
          exit 1
       else
          search_for_strings "COMPILER FAILED TO PRODUCE ERRORS: \"${pretty_source}\" (${errput})" \
-                            "${errput}" "$ccdiag"
+                            "${errput}" "${ccdiag}"
          if [ $? -ne 0 ]
          then
             return 0
@@ -467,8 +467,8 @@ check_test_output()
    local errput
    local output
 
-   output="$3"
-   errput="$4"
+   output="$4"
+   errput="$5"
 
    local rval
 
@@ -489,8 +489,47 @@ check_test_output()
 }
 
 
-run_test_common_preamble()
+#
+# this function defines some quasi-global variables
+# and does some other stuff, it's hackish but....
+#
+#    output
+#    errput
+#    errors
+#    owd
+#    pretty_source
+#
+__preamble()
 {
+   local sourcefile
+   local root
+   local ext
+
+   sourcefile="$1"
+   root="$2"
+   ext="$3"
+
+   local random
+
+   RUNS=`expr "$RUNS" + 1`
+   random=`mktemp -t "${LIBRARY_SHORTNAME}.XXXX"`
+   output="${random}.stdout"
+   errput="${random}.stderr"
+   errors="`basename "${sourcefile}" "${ext}"`.errors"
+
+   owd="`pwd -P`"
+   pretty_source=`relative_path_between "${owd}"/"${sourcefile}" "${root}"`
+
+   if [ "$MULLE_TEST_VERBOSE" = "YES" ]
+   then
+      echo "${pretty_source}" >&2
+   fi
+}
+
+
+run_common_test()
+{
+   local a_out
    local sourcefile
    local ext
    local root
@@ -499,36 +538,23 @@ run_test_common_preamble()
    local stderr
    local ccdiag
 
-   sourcefile="$1"
-   root="$2"
-   ext="$3"
-   stdin="$4"
-   stdout="$5"
-   stderr="$6"
-   ccdiag="$7"
+   a_out="$1"
+   sourcefile="$2"
+   root="$3"
+   ext="$4"
+   stdin="$5"
+   stdout="$6"
+   stderr="$7"
+   ccdiag="$8"
 
    local output
    local errput
    local random
    local fail
    local match
+   local pretty_source
 
-   random=`mktemp -t "${LIBRARY_SHORTNAME}.XXXX"`
-   output="${random}.stdout"
-   errput="${random}.stderr"
-   errors="`basename "${sourcefile}" "${ext}"`.errors"
-
-   local owd
-
-   owd=`pwd`
-   pretty_source=`relative_path_between "${owd}"/"${sourcefile}" "${root}"`
-
-   if [ "$MULLE_TEST_VERBOSE" = "YES" ]
-   then
-      echo "${pretty_source}" >&2
-   fi
-
-   RUNS=`expr "$RUNS" + 1`
+   __preamble "${sourcefile}" "${root}" "${ext}"
 
    # plz2shutthefuckup bash
    set +m
@@ -538,30 +564,81 @@ run_test_common_preamble()
 
    local rval
 
-   if [ "${ext}" = "Makefile" ]
-   then
-      a_out="${owd}/${sourcefile}.exe"
-      run_makefile "${srcfile}" "${owd}" "${a_out}"
-      rval=$?
-   else
-      a_out="${owd}/`basename -- "${sourcefile}" "${ext}"`.exe"
-      run_compiler "${srcfile}" "${owd}" "${a_out}" "${errput}"
-      rval=$?
-   fi
+   "${TEST_BUILDER}" "${srcfile}" "${owd}" "${a_out}" "${errput}"
+   rval=$?
 
    check_compiler_output "${ccdiag}" "${errput}" "${rval}" "${pretty_source}"
 
    run_a_out "${a_out}" < "${stdin}" 2> "${errput}" | ${CRLFCAT} > "${output}"
    rval=$?
 
-   check_test_output  "${stdout}" "${errors}" "${output}" "${errput}" "${rval}" "${pretty_source}" "${a_out}" "${ext}"
+   check_test_output  "${stdout}" \
+                      "${stderr}" \
+                      "${errors}" \
+                      "${output}" \
+                      "${errput}" \
+                      "${rval}"   \
+                      "${pretty_source}" \
+                      "${a_out}" \
+                      "${ext}"
    rval=$?
 
    if [ "${rval}" -ne 0 ]
    then
-      fail_c_test "${sourcefile}" "${a_out}" "${stdin}" "${ext}"
+      "${FAIL_TEST}" "${sourcefile}" "${a_out}" "${stdin}" "${ext}"
    fi
 }
+
+
+run_makefile_test()
+{
+   local sourcefile
+
+   sourcefile="$1"
+
+   local a_out
+   local owd
+
+   owd="`pwd -P`"
+   a_out="${owd}/${sourcefile}.exe"
+
+   TEST_BUILDER=run_makefile
+   FAIL_TEST=fail_test_makefile
+   run_common_test "${a_out}" "$@"
+}
+
+
+run_c_test()
+{
+   local sourcefile
+   local ext
+
+   sourcefile="$1"
+   ext="$3"
+
+   local a_out
+   local owd
+
+   owd="`pwd -P`"
+   a_out="${owd}/`basename -- "${sourcefile}" "${ext}"`.exe"
+
+   TEST_BUILDER=run_compiler
+   FAIL_TEST=fail_test_c
+   run_common_test "${a_out}" "$@"
+}
+
+
+run_m_test()
+{
+   run_c_test "$@"
+}
+
+
+run_cpp_test()
+{
+   log_error "$1: cpp testing is not available yet"
+}
+
 
 
 run_test()
@@ -623,15 +700,15 @@ run_test()
    fi
 
    ccdiag="$1.ccdiag"
-   if [ ! -f "$ccdiag" ]
+   if [ ! -f "${ccdiag}" ]
    then
       ccdiag="expect/$1.ccdiag"
    fi
-   if [ ! -f "$ccdiag" ]
+   if [ ! -f "${ccdiag}" ]
    then
       ccdiag="default.ccdiag"
    fi
-   if [ ! -f "$ccdiag" ]
+   if [ ! -f "${ccdiag}" ]
    then
       ccdiag="-"
    fi
@@ -645,19 +722,19 @@ run_test()
 
    case "${ext}" in
       Makefile)
-         run_makefile_test "$sourcefile" "$root" "$ext" "${stdin}" "${stdout}" "${stderr}" "$ccdiag"
+         run_makefile_test "$sourcefile" "${root}" "${ext}" "${stdin}" "${stdout}" "${stderr}" "${ccdiag}"
          ;;
 
       .m|.aam)
-         run_m_test "$sourcefile" "$root" "$ext" "${stdin}" "${stdout}" "${stderr}" "$ccdiag"
+         run_m_test "$sourcefile" "${root}" "${ext}" "${stdin}" "${stdout}" "${stderr}" "${ccdiag}"
          ;;
 
       .c)
-         run_c_test "$sourcefile" "$root" "$ext" "${stdin}" "${stdout}" "${stderr}" "$ccdiag"
+         run_c_test "$sourcefile" "${root}" "${ext}" "${stdin}" "${stdout}" "${stderr}" "${ccdiag}"
          ;;
 
       .cxx|.cpp)
-         run_cpp_test "$sourcefile" "$root" "$ext" "${stdin}" "${stdout}" "${stderr}" "$ccdiag"
+         run_cpp_test "$sourcefile" "${root}" "${ext}" "${stdin}" "${stdout}" "${stderr}" "${ccdiag}"
          ;;
    esac
 }
@@ -675,7 +752,7 @@ scan_current_directory()
 
    if [ -f Makefile ]
    then
-      run_test "" "$root" "Makefile"
+      run_test "" "${root}" "Makefile"
       return 0
    fi
 
@@ -686,17 +763,17 @@ scan_current_directory()
             ;;
 
          *)
-            if [ -d "$i" ]
+            if [ -d "${i}" ]
             then
-               dir=`pwd`
-               cd "$i"
-               scan_current_directory "$root"
-               cd "$dir"
+               dir="`pwd -P`"
+               cd "${i}"
+               scan_current_directory "${root}"
+               cd "${dir}"
             else
                for ext in ${SOURCE_EXTENSION}
                do
-                  filename=`basename "$i" "${ext}"`
-                  if [ "$filename" != "$i" ]
+                  filename=`basename "${i}" "${ext}"`
+                  if [ "$filename" != "${i}" ]
                   then
                      run_test "${filename}" "${root}" "${ext}"
                      break
@@ -746,27 +823,8 @@ EOF
 }
 
 
-run_all_tests()
-{
-   local directory
 
-   directory=${1:-`pwd`}
-   [ $# -ne 0 ] && shift
-
-   cd "${directory}"
-   scan_current_directory "`pwd -P`"
-
-   if [ "$RUNS" -ne 0 ]
-   then
-      echo "All tests ($RUNS) passed successfully"
-   else
-      echo "no tests found" >&2
-      exit 1
-   fi
-}
-
-
-run_argument_tests()
+run_named_test()
 {
    local directory
    local found
@@ -775,11 +833,11 @@ run_argument_tests()
    local old
    local ext
 
-   directory=`dirname "$TEST"`
-   filename=`basename "$TEST"`
+   directory=`dirname "$1"`
+   filename=`basename "$1"`
    found="NO"
 
-   if [ ! -f "${TEST}" ]
+   if [ ! -f "${1}" ]
    then
       echo "error: source file not found" >&2
       exit 1
@@ -805,11 +863,31 @@ run_argument_tests()
    old="`pwd -P`"
 
       cd "${directory}" || exit 1
-      run_test "${filename}" "${old}" "${ext}"
+      run_test "${name}" "${old}" "${ext}"
       rval=$?
 
    cd "${old}" || exit 1
    exit ${rval}
+}
+
+
+run_all_tests()
+{
+   local directory
+
+   directory=${1:-`pwd`}
+   [ $# -ne 0 ] && shift
+
+   cd "${directory}"
+   scan_current_directory "`pwd -P`"
+
+   if [ "$RUNS" -ne 0 ]
+   then
+      echo "All tests ($RUNS) passed successfully"
+   else
+      echo "no tests found" >&2
+      exit 1
+   fi
 }
 
 
@@ -822,11 +900,8 @@ main()
 
    if [ "`basename "$executable"`" = "run-all-tests" ]
    then
-      TEST=""
+      RUN_ALL="YES"
       MULLE_TEST_VERBOSE="${MULLE_TEST_VERBOSE:-YES}"
-   else
-      TEST="$1"
-      [ $# -ne 0 ] && shift
    fi
 
    #
@@ -836,7 +911,15 @@ main()
    do
       case "$1" in
          -v|--verbose)
-            VERBOSE=YES
+            VERBOSE="YES"
+         ;;
+
+         -V)
+            MULLE_EXECUTOR_TRACE="YES"
+         ;;
+
+         -n)
+            MULLE_EXECUTOR_DRY_RUN="YES"
          ;;
 
          -q)
@@ -877,50 +960,39 @@ main()
    RESTORE_CRASHDUMP=`suppress_crashdumping`
    trap 'trace_ignore "${RESTORE_CRASHDUMP}"' 0 5 6
 
-   # find runtime and headers
-   #
-   # this is more or less an ugly hack, that should work
-   # if
-   #    a) you used mulle-clang-install
-   # or
-   #    b) have a setup like mine
-   #
-   #        ./mulle-clang-install/tests
-   #        ./mulle-objc-runtime
-   #
+   [ -z "${LIBRARY_SHORTNAME}" ] && fail "LIBRARY_SHORTNAME not set"
 
    LIBRARY_FILENAME="${SHLIB_PREFIX}${LIBRARY_SHORTNAME}${STANDALONE_SUFFIX}${SHLIB_EXTENSION}"
    DEPENDENCIES_INCLUDE="../dependencies/include"
    ADDICTIONS_INCLUDE="../addictions/include"
 
-   if [ ! -z "${LIB_PATH}" ]
+   if [ -z "${LIBRARY_PATH}" ]
    then
-      lib="`ls -1 "${LIB_PATH}/${LIBRARY_FILENAME}" 2> /dev/null | tail -1`"
+      if [ ! -z "${LIB_PATH}" ]
+      then
+         LIBRARY_PATH="`ls -1 "${LIB_PATH}/${LIBRARY_FILENAME}" 2> /dev/null | tail -1`"
+      fi
+
+      if [ ! -f "${LIBRARY_PATH}" ]
+      then
+         LIBRARY_PATH="`ls -1 "./lib/${LIBRARY_FILENAME}" 2> /dev/null | tail -1`"
+      fi
+
+      if [ ! -f "${LIBRARY_PATH}" ]
+      then
+         LIBRARY_PATH="`ls -1 "./build/Products/Debug/${LIBRARY_FILENAME}" 2> /dev/null | tail -1`"
+      fi
    fi
-
-
-   if [ ! -f "${lib}" ]
-   then
-      lib="`ls -1 "./lib/${LIBRARY_FILENAME}" 2> /dev/null | tail -1`"
-   fi
-
-   if [ ! -f "${lib}" ]
-   then
-      lib="`ls -1 "./build/Products/Debug/${LIBRARY_FILENAME}" 2> /dev/null | tail -1`"
-   fi
-
-   LIBRARY_PATH="${1:-${lib}}"
-   [ -z $# ] || shift
-
 
    if [ -z "${LIBRARY_PATH}" ]
    then
       log_error "error: ${LIBRARY_FILENAME} can not be found."
 
-      log_info "Maybe you haven't run ./build-test.sh yet ?
+      log_info "Maybe you have not run \"build-test.sh\" yet ?
 
-      You commonly need a shared library target in your CMakeLists.txt that
-      links in all the platform dependencies for your platform."
+You commonly need a shared library target in your CMakeLists.txt that
+links in all the platform dependencies for your platform. This library
+should be installed into \"./lib\" (and headers into \"./include\")"
       exit 1
    fi
 
@@ -981,11 +1053,18 @@ main()
    HAVE_WARNED="NO"
    RUNS=0
 
-   if [ "$TEST" = "" ]
+   if [ "$RUN_ALL" = "YES" ]
    then
       run_all_tests "$@"
    else
-      run_argument_tests "$@"
+
+      [ $# -eq 0 ] && usage
+
+      while [ $# -ne 0 ]
+      do
+         run_named_test "$1"
+         shift
+      done
    fi
 }
 
